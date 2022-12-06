2a074a07c30b810531da6b64d57255668f671bb8
Max Villanueva <maximilianovlara@hotmail.com>
2022-12-06 12:29:09 -0600

    Segundo commit

--- blog.php
+++ blog.php
@@ -11,8 +11,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
+    <link rel="stylesheet" href="diseño.css"/>
 </head>
 <body>
+    <h1>BLOG</h1>
     <br>
     <table border="1">
         <tr>
